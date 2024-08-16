94c5cb8163b507bddac64b73335e4e65b8537f97
RoyceAndrew <royceandrew142@gmail.com>
2024-08-16 16:54:38 +0700

    feature almost finished

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import exios from "axios";
+import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+app.post("/result", (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(port, () => {
