63e8aa2b1dba9c4b499f995c692eb6934a461ac7
RoyceAndrew <royceandrew142@gmail.com>
2024-08-16 15:09:31 +0700

    Currency Converter (title)

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import exios from "axios";
 const port = 3000;
 const app = express();
 
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
 app.listen(port, () => {
     console.log(`Your server has been listened on port ${port}`)
 })
